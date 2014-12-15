3322f9c116e6df078afeb4390b12f1c2d89b609a
José Sazo <jose.sazo@gmail.com>
2014-12-15 16:31:10 -0300

    enable filtering by retail_code and material_code

--- stock/api.py
+++ stock/api.py
@@ -1,7 +1,12 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL
 from stock.models import Product
 
 
 class ProductResource(ModelResource):
     class Meta:
         queryset = Product.objects.filter(in_stock=False)
+        excludes = ['id',]
+        filtering = {
+            'retail_code': ALL,
+            'material_code': ALL,
+        }
